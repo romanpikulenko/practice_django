339c5634b3db7b675f140c576da484588d527a65
Roman Pikulenko <roman.pikulenko@gmail.com>
2023-02-24 13:12:26 +0100

    removed unused import

--- practice_django/settings.py
+++ practice_django/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-from email import message
 from pathlib import Path
 
 from django.contrib.messages import constants as messages
